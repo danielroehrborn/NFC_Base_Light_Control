4d7b8277986116ce627a7e9acfe9135de5c2dfc8
dan <dan066@web.de>
2017-10-10 14:43:36 +0200

    Descriptors

--- NFC_Base_Light_Control/NFC_Base_Light_Control/Tutorial.cpp
+++ NFC_Base_Light_Control/NFC_Base_Light_Control/Tutorial.cpp
@@ -6,6 +6,7 @@ void printdev(libusb_device *dev); //prototype of the function
 
 int tutorial() {
 	libusb_device **devs; //pointer to pointer of device, used to retrieve a list of devices
+	libusb_device_handle *dev_handle = NULL;
 	libusb_context *ctx = NULL; //a libusb session
 	int r; //for return values
 	ssize_t cnt; //holding number of devices in list
@@ -31,20 +32,116 @@ int tutorial() {
 	return 0;
 }
 
+void printDT(libusb_descriptor_type dt) {
+	printf("Descriptor type: ");
+	switch (dt) {
+	case LIBUSB_DT_DEVICE: printf("LIBUSB_DT_DEVICE \n"); break;
+	case LIBUSB_DT_CONFIG: printf("LIBUSB_DT_CONFIG \n"); break;
+	case LIBUSB_DT_STRING: printf("LIBUSB_DT_STRING \n"); break;
+	case LIBUSB_DT_INTERFACE: printf("LIBUSB_DT_INTERFACE \n"); break;
+	case LIBUSB_DT_ENDPOINT: printf("LIBUSB_DT_ENDPOINT \n"); break;
+	case LIBUSB_DT_BOS: printf("LIBUSB_DT_BOS \n"); break;
+	case LIBUSB_DT_DEVICE_CAPABILITY: printf("LIBUSB_DT_DEVICE_CAPABILITY \n"); break;
+	case LIBUSB_DT_HID: printf("LIBUSB_DT_HID \n"); break;
+	case LIBUSB_DT_REPORT: printf("LIBUSB_DT_REPORT \n"); break;
+	case LIBUSB_DT_PHYSICAL: printf("LIBUSB_DT_PHYSICAL \n"); break;
+	case LIBUSB_DT_HUB: printf("LIBUSB_DT_HUB \n"); break;
+	case LIBUSB_DT_SUPERSPEED_HUB: printf("LIBUSB_DT_SUPERSPEED_HUB \n"); break;
+	case LIBUSB_DT_SS_ENDPOINT_COMPANION: printf("LIBUSB_DT_SS_ENDPOINT_COMPANION \n"); break;
+	default: printf("unknown descriptor type \n");
+	}
+}
+
+void printDC(libusb_class_code cc) {
+	printf("Device class: ");
+	switch (cc) {
+	case LIBUSB_CLASS_PER_INTERFACE: printf("LIBUSB_CLASS_PER_INTERFACE \n"); break;
+	case LIBUSB_CLASS_AUDIO: printf("LIBUSB_CLASS_AUDIO \n"); break;
+	case LIBUSB_CLASS_COMM: printf("LIBUSB_CLASS_COMM \n"); break;
+	case LIBUSB_CLASS_HID: printf("LIBUSB_CLASS_HID \n"); break;
+	case LIBUSB_CLASS_PHYSICAL: printf("LIBUSB_CLASS_PER_INTERFACE \n"); break;
+	case LIBUSB_CLASS_PRINTER: printf("LIBUSB_CLASS_PRINTER \n"); break;
+	case LIBUSB_CLASS_IMAGE: printf("LIBUSB_CLASS_IMAGE \n"); break;
+	case LIBUSB_CLASS_MASS_STORAGE: printf("LIBUSB_CLASS_MASS_STORAGE \n"); break;
+	case LIBUSB_CLASS_HUB: printf("LIBUSB_CLASS_HUB \n"); break;
+	case LIBUSB_CLASS_DATA: printf("LIBUSB_CLASS_DATA \n"); break;
+	case LIBUSB_CLASS_SMART_CARD: printf("LIBUSB_CLASS_SMART_CARD \n"); break;
+	case LIBUSB_CLASS_CONTENT_SECURITY: printf("LIBUSB_CLASS_CONTENT_SECURITY \n"); break;
+	case LIBUSB_CLASS_VIDEO: printf("LIBUSB_CLASS_VIDEO \n"); break;
+	case LIBUSB_CLASS_PERSONAL_HEALTHCARE: printf("LIBUSB_CLASS_PERSONAL_HEALTHCARE \n"); break;
+	case LIBUSB_CLASS_DIAGNOSTIC_DEVICE: printf("LIBUSB_CLASS_DIAGNOSTIC_DEVICE \n"); break;
+	case LIBUSB_CLASS_WIRELESS: printf("LIBUSB_CLASS_WIRELESS \n"); break;
+	case LIBUSB_CLASS_APPLICATION: printf("LIBUSB_CLASS_APPLICATION \n"); break;
+	case LIBUSB_CLASS_VENDOR_SPEC: printf("LIBUSB_CLASS_VENDOR_SPEC \n"); break;
+	default: printf("unknown device class \n");
+	}
+}
+
 void printdev(libusb_device *dev) {
 	libusb_device_descriptor desc;
-	int r = libusb_get_device_descriptor(dev, &desc);
-	if (r < 0) {
-		cout << "failed to get device descriptor" << endl;
+	unsigned char data[256];
+	libusb_error ret = (libusb_error)libusb_get_device_descriptor(dev, &desc);
+	if (ret != LIBUSB_SUCCESS) {
+		cout << "failed to get device descriptor: " << libusb_error_name(ret) << endl << libusb_strerror(ret) << endl;
 		return;
 	}
-	cout << "Number of possible configurations: " << (int)desc.bNumConfigurations << endl;
-	cout << "Device Class: " << (int)desc.bDeviceClass << endl;
-	cout << "VendorID: " << desc.idVendor << endl;
+	cout << "Descriptor length: " << desc.bLength << endl;
+	printDT((libusb_descriptor_type)desc.bDescriptorType);
+	printf("USB spec: %X.%X\n", desc.bcdUSB >> 8, (desc.bcdUSB >> 4) & 0xf);
+	cout << "USB spec: " << desc.bcdUSB << endl;
+	printDC((libusb_class_code)desc.bDeviceClass);
+	cout << "DeviceSubClass: " << (int)desc.bDeviceSubClass << endl;
+	cout << "Device protocol: " << desc.bDeviceProtocol << endl;
+	cout << "MaxPacketSize: " << desc.bMaxPacketSize0 << endl;
 	cout << "ProductID: " << desc.idProduct << endl;
+	cout << "VendorID: " << desc.idVendor << endl;
+	cout << "Device release num: " << desc.bcdDevice << endl;
+
+
+	libusb_device_handle* handle;
+	ret = (libusb_error)libusb_open(dev, &handle);
+	if (ret != LIBUSB_SUCCESS) {
+		cout << "failed to open device: " << libusb_error_name(ret) << endl << libusb_strerror(ret) << endl;
+	}
+	else {
+		if (desc.iManufacturer != 0) {
+			ret = (libusb_error)libusb_get_string_descriptor_ascii(handle, desc.iManufacturer, &data[0], 256);
+			if (ret > 0)
+				printf("Manufacturer: %s \n", data);
+			else
+				cout << "failed to read manufacturer string: " << libusb_error_name(ret) << endl << libusb_strerror(ret) << endl;
+		}
+		else
+			cout << "no Manufacturer string" << endl;
+
+		if (desc.iProduct != 0) {
+			ret = (libusb_error)libusb_get_string_descriptor_ascii(handle, desc.iProduct, data, 256);
+			if (ret > 0)
+				printf("Product: %s \n", data);
+			else
+				cout << "failed to read product string: " << libusb_error_name(ret) << endl << libusb_strerror(ret) << endl;
+		}
+		else
+			cout << "no Product string" << endl;
+
+		if (desc.iSerialNumber != 0) {
+			ret = (libusb_error)libusb_get_string_descriptor_ascii(handle, desc.iSerialNumber, data, 256);
+			if (ret > 0)
+				printf("SerialNum: %s \n", data);
+			else
+				cout << "failed to read SerialNum string: " << libusb_error_name(ret) << endl << libusb_strerror(ret) << endl;
+		}
+		else
+			cout << "no SerialNum string" << endl;
+
+		cout << "Number of possible configurations: " << (int)desc.bNumConfigurations << endl;
+
+		libusb_close(handle);
+	}
+
 	libusb_config_descriptor *config;
-	r = libusb_get_config_descriptor(dev, 0, &config);
-	if (r < 0) {
+	ret = (libusb_error)libusb_get_config_descriptor(dev, 0, &config);
+	if (ret < 0) {
 		cout << "failed to get configuration descriptor" << endl;
 		return;
 	}
